7f3f035ef2b3ecc78dfcafc665b84848fabceb86
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-13 09:33:01 +0000

    Add 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching system
+    """
+    def __init__(self):
+        """ Initialize FIFO cache
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item to the cache
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_item = self.order.pop(0)
+                del self.cache_data[first_item]
+                print("DISCARD:", first_item)
+
+        self.cache_data[key] = item
+
+        if key not in self.order:
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key from the cache
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
