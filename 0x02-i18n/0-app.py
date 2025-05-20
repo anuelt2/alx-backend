ff622624e6b3b20d0ef13a368ceb2d0b410e516c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-20 23:01:51 +0000

    Modify 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """A basic Flask app"""
-from flask import Flask, render_template
+
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route("/", methods=["GET"])
-def index():
+def index() -> Response:
     """index route"""
     return render_template("0-index.html")
 
