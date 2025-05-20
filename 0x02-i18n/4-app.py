101719a31382fd7bf9e47b21662516503e99aa25
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-20 16:26:22 +0000

    Add 4-app.py and templates/4-index.html

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""A basic Flask app with Babel config"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """Configuration for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """index route"""
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Checks if an incoming request contains locale argument"""
+    locale = request.args.get("locale")
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
